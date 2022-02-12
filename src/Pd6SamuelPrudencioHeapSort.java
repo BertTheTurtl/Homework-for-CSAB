a0694fad3498a9a481bcdb60ddb3d036a0c7cb93
BertTheTurtl <92934395+BertTheTurtl@users.noreply.github.com>
2022-02-11 23:24:33 -0500

    Part 1 of HeapSort lab is done

--- src/Pd6SamuelPrudencioHeapSort.java
+++ src/Pd6SamuelPrudencioHeapSort.java
@@ -9,8 +9,8 @@
  
  What I am wondering:
  
- The credits: who and/or what website(s) helped you (must state 
- what information you got from the helper or website)
+ The credits: StackOverflow showed me
+              what ternary operators are - https://stackoverflow.com/questions/10336899/what-is-a-question-mark-and-colon-operator-used-for
 
  Students (names) you helped (to what extent, be specific): 
  ****************************************************************************/
@@ -28,15 +28,15 @@ public class Pd6SamuelPrudencioHeapSort
       System.out.println(isSorted(heap));
    
    //Part 2:  Generate 10 random numbers, make a heap, sort it.
-      // int SIZE = 10;
-      // double[] heap = new double[SIZE + 1];
-      // heap = createRandom(heap);
-      // display(heap);
-      // makeHeap(heap);  
-      // display(heap); 
-      // sort(heap);
-      // display(heap);
-      // System.out.println(isSorted(heap));
+      int SIZE = 10;
+      double[] heap = new double[SIZE + 1];
+      heap = createRandom(heap);
+      display(heap);
+      makeHeap(heap);
+      display(heap);
+      sort(heap);
+      display(heap);
+      System.out.println(isSorted(heap));
    }
    
 	//******* Part 1 ******************************************
@@ -52,7 +52,11 @@ public class Pd6SamuelPrudencioHeapSort
    // postcondition:   
    public static void sort(double[] array)
    {
-      
+      for (int i = array.length - 1; i >1; i--)
+      {
+         swap(array, i, 1);
+         heapDown(array, 1, i - 1);
+      }
       
       if(array[1] > array[2])   //just an extra swap, if needed.
          swap(array, 1, 2);
@@ -65,48 +69,44 @@ public class Pd6SamuelPrudencioHeapSort
       array[a] = array[b];
       array[b] = temp;
    }
-   // precondition:
-   // postcondition:
+   // precondition: Given an array, starting integer, and the size of the array
+   // postcondition: Puts in the heap in max-heap order
    public static void heapDown(double[] array, int k, int size)
    {
       int leftChild = k * 2;
       int rightChild = (k * 2) + 1;
-      int biggerChild = leftChild;
-      
-      while(k * 2 >= size)
+
+      if (k > size || leftChild > size)
       {
-         if (leftChild > size || rightChild > size)
+         return;
+      }
+
+      if (rightChild > size)
+      {
+         if (array[k] < array[leftChild])
+            swap(array, k, leftChild);
+      }
+      else
+      {
+         int maxChild = (array[leftChild] > array[rightChild]) ? leftChild : rightChild;
+         if(array[maxChild] > array[k])
          {
-            return;
+            swap(array, k, maxChild);
+            heapDown(array, maxChild, size);
          }
-      
-         if(array[rightChild] != 0 && array[rightChild] > array[leftChild])
-         {
-            biggerChild = rightChild;
-         }
-         if(array[biggerChild] > array[k])
-         {
-            swap(array, k, biggerChild);
-         }
-         
-         k = biggerChild;
-         leftChild = k * 2;
-         rightChild = (k * 2) + 1;
-         biggerChild = leftChild;
-       }
+      }
    }
    // precondition: A given array
-   // postcondition: Will check if the array is sorted in max heap order
+   // postcondition: Will check if the array is sorted in ascending order
    public static boolean isSorted(double[] arr)
    {
-      /*for (int i = 0; i <= (arr.length - 2) / 2; i++) {
-            if (arr[2 * i + 1] > arr[i])
-                return false;
-
-            if (2 * i + 2 < arr.length && arr[2 * i + 2] > arr[i])
-                return false;
-        }
-        return true;*/
+      for(int i = 0; i < arr.length; i++)
+      {
+         if(arr[i] < arr[i+1])
+         {
+            return true;
+         }
+      }
       return false;
    }
    
