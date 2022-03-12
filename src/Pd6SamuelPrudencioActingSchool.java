43ceabd05d38160ee7feff35db87420bd8cdcad3
BertTheTurtl <92934395+BertTheTurtl@users.noreply.github.com>
2022-03-11 20:03:46 -0500

    Finished Acting School assignment

--- src/Pd6SamuelPrudencioActingSchool.java
+++ src/Pd6SamuelPrudencioActingSchool.java
@@ -1,9 +1,10 @@
 //Name: Samuel Prudencio
 //Date: 03/09/22
-// What I learned:
-// How I feel about this lab:
-// What I wonder:
-// Credits: 
+// What I learned: How to iterate through maps and transfering information from one map to another
+// How I feel about this lab: Pretty good, got it done quickly.
+// What I wonder: If Java didn't implement maps, I would have to create two lists. One for the keys and the other for
+//                the values themselves.
+// Credits: Guru Uppala and I talked about different ways of tackling the problem
 
 import java.util.*;
 public class Pd6SamuelPrudencioActingSchool
@@ -31,11 +32,42 @@ public class Pd6SamuelPrudencioActingSchool
    
        // What you need to do:
    	// 1. display initial data.  Use an iterator instead of using the built-in toString method of HashMap
-           
+      Iterator<String> iter = sGrades.keySet().iterator();
+      while (iter.hasNext())
+      {
+         String name = iter.next();
+
+         System.out.println(name +" (" +sGrades.get(name) +")");
+      }
+
+      System.out.println();
    	// 2. reverse the map--use TreeMap
-   	
+   	  Map<String, ArrayList<String>> reverse = new TreeMap<>();
+      iter = sGrades.keySet().iterator();
+
+      while (iter.hasNext())
+      {
+         String name = iter.next();
+         ArrayList<String> input = new ArrayList<>();
+
+         if (reverse.get(sGrades.get(name)) != null)
+         {
+            for (int i = 0; i < reverse.get(sGrades.get(name)).size(); i++)
+               input.add(reverse.get(sGrades.get(name)).get(i));
+          }
+         input.add(name);
+
+         reverse.put(sGrades.get(name), input);
+      }
        // 3. display the reversed map
-      
+      Iterator<String> reverseIter = reverse.keySet().iterator();
+
+      while (reverseIter.hasNext())
+      {
+         String grade = reverseIter.next();
+
+         System.out.println(grade +": " +reverse.get(grade));
+      }
    } // main
 } // Pd6SamuelPrudencioActingSchool
 
